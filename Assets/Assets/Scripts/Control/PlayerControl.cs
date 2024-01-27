835d6296cd698ffab75fa7aa1396400b99f24697
AdrianLloyd7113 <adrian-lloyd@outlook.com>
2024-01-27 17:06:55 -0400

    Built room 1, created prefabs and models in the process

---
+++ Assets/Assets/Scripts/Control/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class PlayerControl : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        //TODO: Keyboard/mouse movement
+
+        //VR movement
+        bool triggerValue;
+        if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primary2DAxis, out triggerValue) && triggerValue){
+            
+        }
+    }
+}
